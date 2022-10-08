93d571f881dc6b925078bc7bfc614eddcb165314
jesusi3 <js508102@gmail.com>
2022-10-08 04:52:32 -0500

    trouble shooting page transitions

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+
 import { Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import {motion, AnimatePresence} from 'framer-motion';
 import HomePage from './HomePage';
 import ProjectPage from './ProjectPage';
 import LandingPage from './LandingPage';
@@ -8,25 +9,41 @@ import LandingPage from './LandingPage';
 import './App.css';
 
 export default function App() {
-    // const [ page, setPage] = useState(false);
-    const [ page, setPage] = useState(false);
+
     return (
-        <div className="App">
+        <AnimatePresence exitBeforeEnter>
+        <motion.div 
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 0.75,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          },
+          animateState: {
+            opacity: 1,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          },
+          exitState: {
+            clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+          },
+        }}
+        className="App"
+        >
             <header className="App-header">
-                <Link to='/'><h1>Portfolio</h1>
-                </Link>
-                
+                <Link to='/'><h1>Portfolio</h1></Link>
             </header>
             <Routes>
                 <Route path="/" element={<LandingPage />} ></Route>
                 <Route path="/about" element={<HomePage />} ></Route>
                 <Route path="/project" element={<ProjectPage />} ></Route>
             </Routes>
-            {/* { page ?
-                <ProjectPage setPage={setPage} page={page}/>:
-                <HomePage setPage={setPage} page={page}/>
 
-            } */}
-        </div>
+        </motion.div>
+        </AnimatePresence>
     );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,11 @@
 import { Link } from 'react-router-dom';
-import {motion, AnimatePresence} from 'framer-motion';
 import './LandingPage.css';
 
 export default function LandingPage() {
     return (
         <div className='LandingPage'>
-            <Link to='/about'>
-                <div>Hello</div>
-            </Link>
+            
+            <Link to='/about'> <div>Hello</div> </Link>
         </div>
     );
 }

2cfa1bf30565c16879c094cdb06d9063ad5b591c
jesusi3 <js508102@gmail.com>
2022-10-12 01:56:35 -0500

    project paage progress part 2

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,7 +2,7 @@ import './AboutMe.css';
 export default function AboutMe () {
     return (
         <div className='AboutMe'>
-            <h1 className='AboutMe-1'>Jesus Sanchez</h1>
+            <h1 className='AboutMe-1'>Hello, I'm Jesus Sanchez</h1>
             <h3 className='AboutMe-1'>Software Engineer</h3>
         </div>
 

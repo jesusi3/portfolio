b4cc2f4fa135ea29aaf1d4b0349d56c56e4bcfd9
jesusi3 <js508102@gmail.com>
2022-10-17 00:17:37 -0500

    Added NameNav

---
+++ src/components/NameNav/NameNav.jsx
@@ -0,0 +1,10 @@
+import './NameNav.css'
+export default function NameNav() {
+    return (
+        <ul className='NameNav'>
+            <li>Comic-Trio</li>
+            <li>Save the Date</li>
+            <li>GreenPill</li>
+        </ul>
+    );
+}

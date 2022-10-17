611c084190a294a10798ed0ad447f31f25bee121
jesusi3 <js508102@gmail.com>
2022-10-17 06:59:16 -0500

    created my data for efficeency

---
+++ src/Data.js
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        Title: "Comic-Trio",
+        Description: "Exress App",
+        TechStack: 'Express',
+        Images: [ 'img', 'img']
+    },
+    {
+        Title: "Save The Date",
+        Description: "Django App",
+        TechStack: 'Django',
+        Images: [ 'img', 'img']
+    },
+    {
+        Title: "Green Pill",
+        Description: "React App",
+        TechStack: "Mern stack",
+        Images: [ "img", "img"]
+    }
+]

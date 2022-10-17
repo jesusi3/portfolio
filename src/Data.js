23a9b4fc18d7d5f21d5cbfc75f0426809c789954
jesusi3 <js508102@gmail.com>
2022-10-17 08:30:58 -0500

    updated the data for projects need to upload photos

--- src/Data.js
+++ src/Data.js
@@ -1,20 +1,20 @@
 export const projects = [
     {
         Title: "Comic-Trio",
-        Description: "Exress App",
-        TechStack: 'Express',
+        Description: "Comic-Trio is a FullStack application where you have a personal Library to do as you please. To access your Library login with your Google email on the top right. When adding a Manga to your library, make sure you keep record by assuring its in your library.",
+        TechStack: 'MongoDB, Mongoose, Express.js, Node.js, CSS, Bootstrap ',
         Images: [ 'img', 'img']
     },
     {
         Title: "Save The Date",
-        Description: "Django App",
-        TechStack: 'Django',
+        Description: "Save the Date is a browser-based social application for recording memories. After creating an account, the user is welcomed with features, such as being able to randomly generate activity ideas. The user creates date post documenting the details of memorable ventures.",
+        TechStack: 'Python, Django, PostgreSQL, CSS, Materialize, AWS Boto s3',
         Images: [ 'img', 'img']
     },
     {
         Title: "Green Pill",
-        Description: "React App",
-        TechStack: "Mern stack",
+        Description: "Green Pill is a Full stack browser-based E-Commerce application for purchases gym related items. Functionality includes adding items to your cart and view previous orders, aswell as toggling through different categries through updating state",
+        TechStack: "React.js, Javascript, Node.js, Express, Mongoose, MongoDB, JSX, AJAX, CSS",
         Images: [ "img", "img"]
     }
 ];
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,7 +1,7 @@
 // import {motion} from 'framer-motion';
 import { useState,useEffect, useRef } from 'react';
 import NameNav from '../components/NameNav/NameNav';
-// import ProjectDetail from '../components/ProjectDetail/ProjectDetail';
+import ProjectDetail from '../components/ProjectDetail/ProjectDetail';
 import './ProjectPage.css';
 
 
@@ -9,9 +9,9 @@ import './ProjectPage.css';
 export default function ProjectPage({projects}) {
     const [projectName, setProjectName] = useState('');
     const TitleRef = useRef([]);
+    const projectdetail = projects.map( (project, idx) => <ProjectDetail key={idx} project={project}  />)
 
     useEffect( function (){
-        // const project = projects.map( (project, idx) => <ProjectDetail key={idx} project={project}  />)
         TitleRef.current = projects.map( project => project.Title);;
         setProjectName(TitleRef.current[0])
     }, [projects]);
@@ -22,9 +22,7 @@ export default function ProjectPage({projects}) {
             <div className='ProjectPage'>
                 <NameNav projects={TitleRef.current} projectName={projectName} setProjectName={setProjectName} />
                 <div className='curved'>
-                    <h1>Green-Pill</h1>
-                    {/* {project} */}
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quisquam obcaecati commodi, perferendis accusamus labore corporis et eligendi sed soluta rerum nam! Voluptates facilis fugit temporibus dolore nobis delectus ducimus.</p>
+                    {projectdetail}
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1400 170" className='svg'><path fill="#0499F5" fillOpacity="1" d="M0,160L60,138.7C120,117,240,75,360,80C480,85,600,139,720,144C840,149,960,107,1080,96C1200,85,1320,107,1380,117.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
                     <div className='Project-Names'>
                         

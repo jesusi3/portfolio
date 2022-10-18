26bdb88080a448af5fad31ba74a56016b308742f
jesusi3 <js508102@gmail.com>
2022-10-17 20:18:10 -0500

    finsihed skills section

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { SiGithub, SiLinkedin } from "react-icons/si";
 import './Nav.css';
 
 export default function Nav ({home, setHome}) {
@@ -10,9 +11,9 @@ export default function Nav ({home, setHome}) {
             <div className='Nav-flex-end'>
                 <div><Link to='/about'>AboutMe</Link></div>
                 <div><Link to='/project'>Projects</Link></div>
-                <div><Link to='/skills'>Skills</Link></div>
-                <div>LinkedIn </div>
-                <div>Github </div>
+                {/* <div><Link to='/skills'>Skills</Link></div> */}
+                <div><SiLinkedin /> </div>
+                <div><SiGithub/>  </div>
             </div>
         </div >
     );

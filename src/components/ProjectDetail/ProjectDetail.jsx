1914a5c92ed950a297e1787fe6d12aa420ba4f69
jesusi3 <js508102@gmail.com>
2022-10-18 04:32:32 -0500

    more trouble shoting for images

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -1,11 +1,16 @@
 import './ProjectDetail.css';
 export default function ProjectDetail({projDet, projectName}) {
     // console.log(projDet);
-    const ims = projDet.map( x => x.Images[0]);
-    console.log(ims)
+    const four = projDet.map( x => x.Images);
+    console.log('four',four[0])
+    // const three = four.map(x => <div>{x}</div>);
+    const three = four.map(x => <img src={x} alt="Green-Neworderpage"/>);
+    console.log('three', three[0]);
+    // console.log('three', three[0]);
     return(
         <div className='Image'>
-            {/* <img src={images[0]} alt="Green-Neworderpage"/> */}
+            {/* <img src={three} alt="Green-Neworderpage"/> */}
+            {three}
         </div>
     );
 

fbed54143fe5633905ce1723695bb0f156d1df01
jesusi3 <js508102@gmail.com>
2022-10-28 02:03:56 -0500

    troubleshooting

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -1,14 +1,22 @@
+import { useState, useEffect } from 'react';
+
 import './ProjectDetail.css';
 export default function ProjectDetail({projDet, projectName}) {
-    // console.log(projDet);
-    const four = projDet.map( x => x.Images);
-    console.log('four',four[0])
-    const two = four[0];
-    const three = two.map((x, idx) => <img key={idx} src={x} alt={idx} />);
+    const [img, setImg] = useState([]);
+    useEffect(function () {
+        async function tester () {
+            const look = await projDet.map( x => x.Images);
+            const hi = await look[0];
+            const pep = await hi.map((x, idx) => x);
+            setImg(pep);
+            console.log('out',img);
+        }
+        tester();
+    }, [projDet]);
     return(
         <div className='Image'>
-            {/* <img src={three} alt="Green-Neworderpage"/> */}
-            {three}
+            
+            {/* <button onClick={tester}>hello</button> */}
         </div>
     );
 
---
+++ src/components/ProjectDetail/random.jsx
@@ -0,0 +1,10 @@
+
+
+export default function random () {
+
+    return (
+        <>
+        <div> </div>
+        </>
+    );
+}

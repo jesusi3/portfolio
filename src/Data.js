1a94f2d13a403838c0537637658e2cce36187481
jesusi3 <js508102@gmail.com>
2022-10-28 05:31:21 -0500

    fixing more problems

--- src/Data.js
+++ src/Data.js
@@ -1,21 +1,21 @@
 export const projects = [
     {
-        Title: "Comic-Trio",
-        Description: "Comic-Trio is a FullStack application where you have a personal Library to do as you please. To access your Library login with your Google email on the top right. When adding a Manga to your library, make sure you keep record by assuring its in your library.",
+        Title: 'Comic-Trio',
+        Description: 'Comic-Trio is a FullStack application where you have a personal Library to do as you please. To access your Library login with your Google email on the top right. When adding a Manga to your library, make sure you keep record by assuring its in your library.',
         TechStack: 'MongoDB, Mongoose, Express.js, Node.js, CSS, Bootstrap ',
-        Images: [ 'https://i.gyazo.com/781d6aa6e27551431cd8f3e4c346dfae.png', 'https://i.gyazo.com/a370c65d46c77d3b724b2cbe49d7f4d0.png', 'https://i.gyazo.com/2dcd62b652d7fab7b65dd8ae474c99a9.png']
+        Images: 'https://i.gyazo.com/a370c65d46c77d3b724b2cbe49d7f4d0.png',
     },
     {
-        Title: "Save The Date",
-        Description: "Save the Date is a browser-based social application for recording memories. After creating an account, the user is welcomed with features, such as being able to randomly generate activity ideas. The user creates date post documenting the details of memorable ventures.",
+        Title: 'Save The Date',
+        Description: 'Save the Date is a browser-based social application for recording memories. After creating an account, the user is welcomed with features, such as being able to randomly generate activity ideas. The user creates date post documenting the details of memorable ventures.',
         TechStack: 'Python, Django, PostgreSQL, CSS, Materialize, AWS Boto s3',
-        Images: [ 'https://i.gyazo.com/5d809128f6bef83744a527820863e613.png', 'https://i.gyazo.com/b371aed78ea3a8c85acd81f7a359280f.jpg', 'https://i.gyazo.com/4e3f843c9740455c20419eeba2b5137d.png']
+        Images: 'https://i.gyazo.com/b371aed78ea3a8c85acd81f7a359280f.jpg',
     },
     {
-        Title: "Green Pill",
-        Description: "Green Pill is a Full stack browser-based E-Commerce application for purchases gym related items. Functionality includes adding items to your cart and view previous orders, aswell as toggling through different categries through updating state",
-        TechStack: "React.js, Javascript, Node.js, Express, Mongoose, MongoDB, JSX, AJAX, CSS",
-        Images: [ "https://i.gyazo.com/8d828b806b3cd6fe272f45b47fa6706c.png", "https://i.gyazo.com/5731ea24aba6c057513895934d04282b.png"]
+        Title: 'Green Pill',
+        Description: 'Green Pill is a Full stack browser-based E-Commerce application for purchases gym related items. Functionality includes adding items to your cart and view previous orders, aswell as toggling through different categries through updating state',
+        TechStack: 'React.js, Javascript, Node.js, Express, Mongoose, MongoDB, JSX, AJAX, CSS',
+        Images: "https://i.gyazo.com/8d828b806b3cd6fe272f45b47fa6706c.png",
     }
 
 ];
--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -1,21 +1,11 @@
-import { useState, useEffect } from 'react';
-
+import {useState, useEffect} from 'react';
 import './ProjectDetail.css';
 export default function ProjectDetail({projDet, projectName}) {
-    const [img, setImg] = useState([]);
-    useEffect(function () {
-        async function tester () {
-            const look = await projDet.map( x => x.Images);
-            const hi = await look[0];
-            const pep = await hi.map((x, idx) => x);
-            setImg(pep);
-            console.log('out',img);
-        }
-        tester();
-    }, [projDet]);
+    console.log('det',projDet[0])
+    // const hello = projDet
     return(
         <div className='Image'>
-            
+            {/* <img src={projDet[0].Images} alt="" /> */}
             {/* <button onClick={tester}>hello</button> */}
         </div>
     );
